8f65ba63f014ddd196a663146f4782dbb559b69b
Alexandro <alexandro.gomes@imusica.com.br>
2015-09-16 16:41:45 -0300

    ajustes de celula utilizando xib

---
+++ Example/MenuItemTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MenuItemTableViewCell.h
+//  Example
+//
+//  Created by Alexandro on 9/16/15.
+//  Copyright (c) 2015 Robert Nash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuItemTableViewCell : UITableViewCell
+
+@end
---
+++ Example/MenuItemTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MenuItemTableViewCell.m
+//  Example
+//
+//  Created by Alexandro on 9/16/15.
+//  Copyright (c) 2015 Robert Nash. All rights reserved.
+//
+
+#import "MenuItemTableViewCell.h"
+
+@implementation MenuItemTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
