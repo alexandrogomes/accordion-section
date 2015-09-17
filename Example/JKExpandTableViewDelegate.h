ffc7cd6edf5d787cf6c74b0411d774fa1005b78b
Alexandro <alexandro.gomes@imusica.com.br>
2015-09-17 18:07:04 -0300

    criando protocolo para selecionar

---
+++ Example/JKExpandTableViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  JKExpandTableViewDelegate.h
+//  Example
+//
+//  Created by Alexandro on 9/17/15.
+//  Copyright (c) 2015 Robert Nash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol JKExpandTableViewDelegate <NSObject>
+
+@end
